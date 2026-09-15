a32ac7676079437c427b665e4b1723372162f4fb
Delton Mukoya Kuya <deltonmukoyakuya@gmail.com>
2026-09-15 10:32:57 +0300

    Register lucide template library in TEMPLATES setting

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,10 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
+            "libraries": {
+                "lucide": "core.templatetags.lucide",
+            },
         },
     }
 ]
