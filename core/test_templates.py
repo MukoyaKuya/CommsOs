ef3fcb9bc636297934f4bd596c42c52c8554754e
Delton Mukoya Kuya <deltonmukoyakuya@gmail.com>
2026-09-16 09:05:26 +0300

    feat: redesign public landing page

--- core/test_templates.py
+++ core/test_templates.py
@@ -8,3 +8,15 @@ class PublicTemplateTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'aria-label="CommsOS Logo"')
+
+    def test_landing_page_exposes_three_role_dashboard_previews(self):
+        response = self.client.get(reverse("landing"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'role="tablist"')
+        self.assertContains(response, 'role="tab"', count=3)
+        self.assertContains(response, 'role="tabpanel"', count=3)
+        self.assertContains(response, "Communications Officer")
+        self.assertContains(response, "Support Staff")
+        self.assertContains(response, "Intern")
+        self.assertContains(response, reverse("login"))
